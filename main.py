704d4919415e6f5c369b6d3f696f7342666f76d1
carlos <carlosdavidfloressilva@gmail.com>
2020-07-06 03:47:42 -0500

    API finished

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 from flask import Flask, request, make_response, redirect
 import tensorflow as tf
-from keras_preprocessing.image import ImageDataGenerator
+from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.models import Sequential, load_model
 from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Input, Dropout, Activation
 from keras import backend as X
 from keras.utils import to_categorical
 
 from keras import Model
-from keras.preprocessing import image
-
 import numpy as np
 
 import os
 from os import listdir
 from os.path import isfile, join
 
+from PIL import Image
+import io
+
+import cv2
+import random
 
 app = Flask(__name__)
 
 @app.route('/index')
 def index():
+    
     # Getting image from body request
     file = request.files['file']
-    model = load_model('./rps.h5')
-    return "{}".format(type(file))
+    
+    # load model
+    model = load_model('./rps-2.h5')
+    
+    #compile model
+    model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    
+    # proccessing request body image
+
+    img = Image.open(file)
+    rgb_im = img.convert('RGB')
+    path = "./img/{}.jpg".format(random.randint(1,100000))
+    rgb_im.save(path)
+    img = load_img(path, target_size=(150, 150))
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+
+    return "{}".format(classes)
 
  
