77c443b7f8689ada1c9450b1234f130eeec3333a
carlos <carlosdavidfloressilva@gmail.com>
2020-07-07 01:35:34 -0500

    Finished api with jinja 2 and bootstrap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template, url_for
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.models import Sequential, load_model
@@ -18,33 +18,57 @@ import io
 
 import random
 
+from flask_bootstrap import Bootstrap
+...
+
 app = Flask(__name__)
 
-@app.route('/index', methods=["POST"])
+bootstrap = Bootstrap(app)
+    
+@app.errorhandler(404)
+def notFound(error):
+    return render_template("404.html", error = error)
+
+@app.route('/index', methods=["GET", "POST"])
 def index():
-    
-    # Getting image from body request
-    file = request.files['file']
-    
-    # load model
-    model = load_model('./rps-2.h5')
-    
-    #compile model
-    model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-    
-    # proccessing request body image
+    if request.method == "GET":
+        return render_template("complete.html")
+    else:
+        return redirect(url_for("index"))
 
-    img = Image.open(file)
-    rgb_im = img.convert('RGB')
-    path = "./img/{}.jpg".format(random.randint(1,100000))
-    rgb_im.save(path)
-    img = load_img(path, target_size=(150, 150))
-    x = img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    images = np.vstack([x])
-    classes = model.predict(images, batch_size=10)
+@app.route('/', methods=["GET","POST"])
+def rockPaperScissors():
 
-    return "{}".format(classes)
+    if request.method == "GET":
+        return render_template("complete.html")
+
+    else:
+    
+        # Getting image from body request
+        file = request.files['file']
+        
+        # load model
+        model = load_model('./rps-2.h5')
+        
+        #compile model
+        model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+        
+        # proccessing request body image
+
+        img = Image.open(file)
+        rgb_im = img.convert('RGB')
+        path = "./img/{}.jpg".format(random.randint(1,100000))
+        rgb_im.save(path)
+        img = load_img(path, target_size=(150, 150))
+        x = img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        images = np.vstack([x])
+        classes = model.predict(images, batch_size=10)
+
+        predictClass = classes[0].tolist()
+        predictClass = predictClass.index(max(predictClass))
+
+        return render_template("complete.html", classes = predictClass)
 
 port = int(os.environ.get('PORT', 5000))
 
